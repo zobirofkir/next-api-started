0be9d1451889fcf344b7582ea2659a864a869415
csw <zobirofkir19@gmail.com>
2025-09-30 12:47:18 +0100

    create User Model

---
+++ app/lib/models/User.js
@@ -0,0 +1,9 @@
+import { Schema } from 'mongoose';
+
+const UserSchema = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}, { timestamps: true });
+
+export default mongoose.models.User || mongoose.model('User', UserSchema);
