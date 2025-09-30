6185ec56b7ed25b27be0e9b829cad9da6cb7194a
csw <zobirofkir19@gmail.com>
2025-09-30 13:00:42 +0100

    show the user

--- app/lib/controllers/UserController.js
+++ app/lib/controllers/UserController.js
@@ -2,9 +2,22 @@ import BaseController from "./BaseController";
 import User from "@/app/lib/models/User";
 
 class UserController extends BaseController {
-    static async index() {
+    /**
+     * List All Users
+     */
+    static async index() 
+    {
         return await this.withConnection(() => User.find());
     }
+
+    /**
+     * Find the user by id
+     */
+    static async show(id)
+    {
+        return await this.withConnection(() => User.findById(id));
+    }
+
 }
 
 export default UserController;
