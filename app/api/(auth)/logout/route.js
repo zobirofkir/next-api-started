3e0504a2c70ed651c17ab1fbf7d0ef7bcd92a07b
csw <zobirofkir19@gmail.com>
2025-10-01 10:57:28 +0100

    make logout protected route

--- app/api/(auth)/logout/route.js
+++ app/api/(auth)/logout/route.js
@@ -1,11 +1,17 @@
 import { AuthController } from "@/app/lib/controllers/AuthController";
 import { NextResponse } from "next/server";
+import requireAuth from "@/app/lib/auth/requireAuth.js";
 
 export const POST = async (request) => {
-    const req = {};
-    const res = {
-        json: (data) => NextResponse.json(data)
-    };
-    
-    return AuthController.logout(req, res);
+    return requireAuth(request, async (user) => {
+        const req = { user };
+        const res = {
+            status: (code) => ({
+                json: (data) => NextResponse.json(data, { status: code })
+            }),
+            json: (data) => NextResponse.json(data)
+        };
+
+        return AuthController.logout(req, res);
+    });
 }
