3293df78b8636ee3c6c38c2920a22fad13bdc5a3
csw <zobirofkir19@gmail.com>
2025-10-02 09:28:36 +0100

    update

--- app/lib/resources/FacilityResource.js
+++ app/lib/resources/FacilityResource.js
@@ -6,7 +6,7 @@ class FacilityResource extends Resource {
             id: this.resource._id,
             name: this.resource.name,
             sport: this.resource.sport,
-            image: process.env.APP_URL + this.resource.image,
+            image: "https://university-frontend-eight.vercel.app/" + this.resource.image,
             capacity: this.resource.capacity,
             price: this.resource.price,
             rating: this.resource.rating,
