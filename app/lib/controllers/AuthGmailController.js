b6531d1aefa661ee2708fbd648291b0028d5a30d
csw <zobirofkir19@gmail.com>
2025-10-03 21:00:53 +0100

    clean auth gmail controller

--- app/lib/controllers/AuthGmailController.js
+++ app/lib/controllers/AuthGmailController.js
@@ -4,22 +4,56 @@ import { auth, googleProvider } from "../../firebase/config";
 import User from "../models/User";
 import AuthGmailResource from "../resources/AuthGmailResource";
 
+/**
+ * @class AuthGmailController
+ * @extends BaseController
+ * @description Handles Google OAuth authentication using Firebase
+ */
 class AuthGmailController extends BaseController {
+    /**
+     * @constructor
+     * @description Initializes the controller with the AuthGmailResource
+     */
     constructor() {
         super();
         this.resource = AuthGmailResource;
     }
 
+    /**
+     * @async
+     * @method login
+     * @description Authenticates a user using Google OAuth
+     * @param {Object} req - Express request object
+     * @param {Object} res - Express response object
+     * @returns {Promise<Object>} Returns user data with JWT token on success
+     * @throws {Error} Throws error if authentication fails
+     * 
+     * @example
+     * // Successful response
+     * {
+     *   success: true,
+     *   message: 'Login successful',
+     *   data: {
+     *     user: {
+     *       id: 'user_id',
+     *       name: 'John Doe',
+     *       email: 'john@example.com',
+     *       photoURL: 'https://...',
+     *       provider: 'google',
+     *       createdAt: '2025-10-03T20:00:00.000Z',
+     *       updatedAt: '2025-10-03T20:00:00.000Z'
+     *     },
+     *     token: 'jwt_token_here'
+     *   }
+     * }
+     */
     async login(req, res) {
         try {
-            // Sign in with Google Popup
             const result = await signInWithPopup(auth, googleProvider);
             const user = result.user;
             
-            // Check if user exists in database
             let dbUser = await User.findOne({ email: user.email });
             
-            // If user doesn't exist, create a new one
             if (!dbUser) {
                 dbUser = new User({
                     name: user.displayName,
@@ -30,20 +64,17 @@ class AuthGmailController extends BaseController {
                 });
                 await dbUser.save();
             } else {
-                // Update user data if they exist
                 dbUser.photoURL = user.photoURL;
                 dbUser.firebaseUid = user.uid;
                 await dbUser.save();
             }
 
-            // Generate JWT token
             const token = this.jwtSign({
                 id: dbUser._id,
                 email: dbUser.email,
                 name: dbUser.name
             });
 
-            // Return user data with token
             return this.response.success(
                 res,
                 {
