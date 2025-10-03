16d4e01aaa6311b8ac746e21bdd052e278d62701
csw <zobirofkir19@gmail.com>
2025-10-03 20:55:17 +0100

    create auth gmail resource

---
+++ app/lib/requests/AuthGmailResource.js
@@ -0,0 +1,23 @@
+import BaseRequest from './BaseRequest.js';
+
+export default class AuthGmailResource extends BaseRequest {
+  rules() {
+    return {
+        //
+    };
+  }
+
+  messages() {
+    return {
+        //
+    };
+  }
+
+  sanitize(values) {
+    return {
+        //
+    };
+  }
+}
+
+
